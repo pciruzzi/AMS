78e06f105bdf145696d32ad7fe064afec09a22de
Pipi <p.ciruzzi@hotmail.com>
2016-12-11 17:07:20 +0100

    Adding Jersey multipart to upload files to server

--- src/java/fr/insa/ams/ws/StudentWS.java
+++ src/java/fr/insa/ams/ws/StudentWS.java
@@ -3,8 +3,15 @@ package fr.insa.ams.ws;
 import fr.insa.ams.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.sun.jersey.multipart.FormDataParam;
 import fr.insa.ams.json.StudentAdapter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
@@ -57,5 +64,33 @@ public class StudentWS {
         Student student = new Student(name, password, email, year, pathway, address, telephone, group);
         int studentId = db.add(student);
         return Response.created(URI.create(String.valueOf(studentId))).build();
+    }
+
+        @POST
+    @Path("/{id}/cvs")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadCV(@HeaderParam("id") int userId, @PathParam("id") int id, @FormDataParam("file") InputStream uploadedInputStream/*,
+                                            @FormDataParam("file") FormDataContentDisposition fileDetail*/) {
+        String fileLocation = "/home/pablo/" + userId + ".pdf";//+ fileDetail.getFileName();
+        System.out.println("Uploading file to " + fileLocation);
+        writeToFile(uploadedInputStream, fileLocation);
+        System.out.println("File written...");
+        return Response.created(URI.create(fileLocation)).build();
+    }
+
+    private void writeToFile(InputStream uploadedInputStream, String fileLocation) {
+        try {
+            OutputStream out = new FileOutputStream(new File(fileLocation));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            out = new FileOutputStream(new File(fileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
